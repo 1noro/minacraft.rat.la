78fe92cdac4986d17f2dcc3dcbeb896bfa93d72c
1noro <ppuubblliicc@protonmail.com>
2020-05-27 18:50:28 +0200

    df

--- install-server.sh
+++ install-server.sh
@@ -7,7 +7,7 @@ mkdir -p /opt/minecraft
 adduser --system --shell /bin/bash --home /opt/minecraft --group minecraft
 cd /opt/minecraft
 git clone https://github.com/1noro/minecraft.rat.la.git
-cd /opt/minecraft/minecraft_rat_la
+cd /opt/minecraft/minecraft.rat.la
 cp minecraft@.service /etc/systemd/system/minecraft@.service
 chown minecraft:minecraft -R /opt/minecraft #necesario
 systemctl enable minecraft@minecraft_rat_la
