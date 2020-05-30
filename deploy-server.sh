6664bbe1e5d41f5bea78d91bf90e861f2357a2cd
1noro <ppuubblliicc@protonmail.com>
2020-05-30 13:36:22 +0200

    jghfhjghj

--- deploy-server.sh
+++ deploy-server.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# RUN AS ROOT!!
-#apt install openjdk-8-jre-headless curl screen nano bash grep git
-#apt install openjdk-* curl screen nano bash grep git
+# RUN AS ROOT
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as root"
+    exit
+fi
+
 apt update
-#apt install default-jre curl screen nano bash grep git wget -y
+# apt upgrade -y
+# apt install default-jre curl screen nano bash grep git wget -y
 apt install openjdk-11-jre-headless curl screen nano bash grep git wget -y
 mkdir -p /opt/minecraft
 adduser --system --shell /bin/bash --home /opt/minecraft --group minecraft
@@ -15,6 +19,5 @@ cd /opt/minecraft/minecraft.rat.la
 wget https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar
 
 cp minecraft@.service /etc/systemd/system/minecraft@.service
-chown minecraft:minecraft -R /opt/minecraft #necesario
+chown minecraft:minecraft -R /opt/minecraft # necesario
 systemctl enable minecraft@minecraft.rat.la
-#systemctl disable minecraft@minecraft.rat.la
